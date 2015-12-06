d2d194958abd3806981db8ce0537547db511f810
Antoine MICHAUD <antoinemichaud34@gmail.com>
2015-12-06 23:02:17 +0100

    Add sets route

--- src/main/java/api/ChallengesApi.java
+++ src/main/java/api/ChallengesApi.java
@@ -1,7 +1,12 @@
 package api;
 
+import com.google.common.base.Splitter;
 import net.codestory.http.WebServer;
 import tennis.TennisService;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 public class ChallengesApi {
 
@@ -30,6 +35,11 @@ public class ChallengesApi {
                                         context.query().getInteger("player1Score"),
                                         context.get("player2Name"),
                                         context.query().getInteger("player2Score")))
+                        .get("/sets/displaySetScore", (context) -> {
+                            List<Integer> scores = Splitter.on(",").splitToList(context.get("scores")).stream().map(Integer::parseInt).collect(toList());
+                            return tennisService.displaySetScore(scores);
+                        })
+
         ).start(8080);
     }
 
