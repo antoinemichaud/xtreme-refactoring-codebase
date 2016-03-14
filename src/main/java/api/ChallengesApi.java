6bd327f2780296adf528059338806a48dccea6e5
Marie-Laure Thuret <mthuret@xebia.fr>
2016-03-14 22:29:51 +0100

    fix: instantiate new TennisService() at each request
    -> score are global variabel
    (cherry picked from commit 52b45b2)

--- src/main/java/api/ChallengesApi.java
+++ src/main/java/api/ChallengesApi.java
@@ -6,11 +6,9 @@ import tennis.TennisService;
 public class ChallengesApi {
 
     public static void main(String[] args) {
-        TennisService tennisService = new TennisService();
-
         new WebServer().configure(
                 routes -> routes
-                        .get("/displayScore", (context) -> tennisService
+                        .get("/displayScore", (context) -> new TennisService()
                                 .displayScore(context.get("player1Name"),
                                         context.query().getInteger("player1Score"),
                                         context.get("player2Name"),
