029a32fbe30da4dcf24065f4fae4caf8872f03f0
Marie-Laure Thuret <mthuret@xebia.fr>
2016-03-22 22:24:32 +0100

    tests: remove parametrized tests

--- src/test/java/tennis/TennisTest.java
+++ src/test/java/tennis/TennisTest.java
@@ -1,79 +1,50 @@
 package tennis;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
 import java.util.Arrays;
 import java.util.Collection;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 
-@RunWith(Parameterized.class)
 public class TennisTest {
 
-    private int player1Score;
-    private int player2Score;
-    private String expectedScore;
+    TennisService game = new TennisService();
 
-    public TennisTest(int player1Score, int player2Score, String expectedScore) {
-        this.player1Score = player1Score;
-        this.player2Score = player2Score;
-        this.expectedScore = expectedScore;
-    }
-
-    @Parameters
-    public static Collection<Object[]> getallScores() {
-        return Arrays.asList(new Object[][] {
-                { 0, 0, "love-all" },
-                { 1, 1, "fifteen-all" },
-                { 2, 2, "thirty-all"},
-                { 3, 3, "deuce"},
-                { 4, 4, "deuce"},
-
-                { 1, 0, "fifteen-love"},
-                { 0, 1, "love-fifteen"},
-                { 2, 0, "thirty-love"},
-                { 0, 2, "love-thirty"},
-                { 3, 0, "forty-love"},
-                { 0, 3, "love-forty"},
-                { 4, 0, "win for player1"},
-                { 0, 4, "win for player2"},
-
-                { 2, 1, "thirty-fifteen"},
-                { 1, 2, "fifteen-thirty"},
-                { 3, 1, "forty-fifteen"},
-                { 1, 3, "fifteen-forty"},
-                { 4, 1, "win for player1"},
-                { 1, 4, "win for player2"},
-
-                { 3, 2, "forty-thirty"},
-                { 2, 3, "thirty-forty"},
-                { 4, 2, "win for player1"},
-                { 2, 4, "win for player2"},
-
-                { 4, 3, "advantage player1"},
-                { 3, 4, "advantage player2"},
-                { 5, 4, "advantage player1"},
-                { 4, 5, "advantage player2"},
-                { 15, 14, "advantage player1"},
-                { 14, 15, "advantage player2"},
-
-                { 6, 4, "win for player1"},
-                { 4, 6, "win for player2"},
-                { 16, 14, "win for player1"},
-                { 14, 16, "win for player2"},
-        });
-    }
-
-    public void checkAllScores(TennisService game) {
-        assertEquals(this.expectedScore, game.displayScore("player1", this.player1Score, "player2", this.player2Score));
+    @Test
+    public void should_display_regular_score() {
+        assertThat(displayScore(1, 0)).isEqualTo("fifteen-love");
+        assertThat(displayScore(1, 2)).isEqualTo("fifteen-thirty");
+        assertThat(displayScore(3, 2)).isEqualTo("forty-thirty");
     }
 
     @Test
-    public void checkAllScoresTennisGame1() {
-        TennisService game = new TennisService();
-        checkAllScores(game);
+    public void should_display_score_for_equality() {
+        assertThat(displayScore(0, 0)).isEqualTo("love-all");
+        assertThat(displayScore(1, 1)).isEqualTo("fifteen-all");
+        assertThat(displayScore(2, 2)).isEqualTo("thirty-all");
+        assertThat(displayScore(3, 3)).isEqualTo("deuce");
+        assertThat(displayScore(4, 4)).isEqualTo("deuce");
+    }
+
+    @Test
+    public void should_display_score_for_advantage() {
+        assertThat(displayScore(3, 4)).isEqualTo("advantage player2");
+        assertThat(displayScore(4, 3)).isEqualTo("advantage player1");
+        assertThat(displayScore(5, 4)).isEqualTo("advantage player1");
+        assertThat(displayScore(14, 15)).isEqualTo("advantage player2");
+    }
+
+    @Test
+    public void should_display_score_for_the_end_of_the_game() {
+        assertThat(displayScore(6, 4)).isEqualTo("win for player1");
+        assertThat(displayScore(4, 6)).isEqualTo("win for player2");
+        assertThat(displayScore(16, 14)).isEqualTo("win for player1");
+    }
+
+    private String displayScore(int player1Score, int player2Score) {
+        return game.displayScore("player1", player1Score, "player2", player2Score);
     }
 }
