0b2b34e8d8516c98f79c312ac48b88670c113fd0
Antoine MICHAUD <antoinemichaud34@gmail.com>
2016-04-18 21:16:54 +0200

    Make server call tennis game code

--- python/tennis/tennis_api.py
+++ python/tennis/tennis_api.py
@@ -1,10 +1,19 @@
 from flask import Flask
+from flask import request
+import tennis.tennis_service
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/displayScore')
+def display_score():
+    return tennis.tennis_service.display_score(int(request.args.get("player1Score")), int(request.args.get("player2Score")))
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
