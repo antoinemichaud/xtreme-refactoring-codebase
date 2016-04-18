a1d7c329c130fd27b3fe15fed3d7c210cd82da3c
Antoine MICHAUD <antoinemichaud34@gmail.com>
2016-04-18 20:43:51 +0200

    Add rule and test for equality

--- python/tennis/tennis_service.py
+++ python/tennis/tennis_service.py
@@ -1,5 +1,19 @@
-def global_score_as_string_for_equality():
-    pass
+def display_score(player1_score, player2_score):
+    if player1_score == player2_score:
+        return global_score_as_string_for_equality(player1_score)
+    elif player1_score >= 4 or player2_score >= 4:
+        return global_score_as_string_for_point_end()
+    else:
+        return single_player_score_as_string(player1_score) + "-" + single_player_score_as_string(player2_score)
+
+
+def global_score_as_string_for_equality(player_score):
+    switcher = {
+        0: "love-all",
+        1: "fifteen-all",
+        2: "thirty-all",
+    }
+    return switcher.get(player_score, "deuce")
 
 
 def global_score_as_string_for_point_end():
@@ -13,12 +27,3 @@ def single_player_score_as_string(player1_score):
         2: "thirty",
     }
     return switcher.get(player1_score, "forty")
-
-
-def display_score(player1_score, player2_score):
-    if player1_score == player2_score:
-        return global_score_as_string_for_equality()
-    elif player1_score >= 4 or player2_score >= 4:
-        return global_score_as_string_for_point_end()
-    else:
-        return single_player_score_as_string(player1_score) + "-" + single_player_score_as_string(player2_score)
--- python/tests/test_tennis.py
+++ python/tests/test_tennis.py
@@ -5,3 +5,11 @@ def should_display_regular_score():
     assert display_score(1, 0) == "fifteen-love"
     assert display_score(1, 2) == "fifteen-thirty"
     assert display_score(3, 2) == "forty-thirty"
+
+
+def should_display_score_for_equality():
+    assert display_score(0, 0) == "love-all"
+    assert display_score(1, 1) == "fifteen-all"
+    assert display_score(2, 2) == "thirty-all"
+    assert display_score(3, 3) == "deuce"
+    assert display_score(4, 4) == "deuce"
