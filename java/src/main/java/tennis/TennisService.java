9ba48aa7ac9ab7eb64b63841f62499fab8ed0ed4
Antoine MICHAUD <antoinemichaud34@gmail.com>
2016-04-23 13:08:02 +0200

    french solution java

--- java/src/main/java/tennis/TennisService.java
+++ java/src/main/java/tennis/TennisService.java
@@ -147,4 +147,99 @@ public class TennisService {
             P2Score();
     }
 
+    public String displayScore(String language, String player1Name, int player1Score, String player2Name, int player2Score) {
+        this.player1Name = player1Name;
+        this.player2Name = player2Name;
+        this.P1point = 0;
+        this.P2point = 0;
+        SetP1Score(player1Score);
+        SetP2Score(player2Score);
+        return getFrenchScore();
+    }
+
+    public String getFrenchScore(){
+        String score = "";
+        if (P1point == P2point && P1point < 4)
+        {
+            if (P1point==0)
+                score = "zéro";
+            if (P1point==1)
+                score = "quinze";
+            if (P1point==2)
+                score = "trente";
+            score += "-partout";
+        }
+        if (P1point==P2point && P1point>=3)
+            score = "égalité";
+
+        if (P1point > 0 && P2point==0)
+        {
+            if (P1point==1)
+                P1res = "quinze";
+            if (P1point==2)
+                P1res = "trente";
+            if (P1point==3)
+                P1res = "quarante";
+
+            P2res = "zéro";
+            score = P1res + "-" + P2res;
+        }
+        if (P2point > 0 && P1point==0)
+        {
+            if (P2point==1)
+                P2res = "quinze";
+            if (P2point==2)
+                P2res = "trente";
+            if (P2point==3)
+                P2res = "quarante";
+
+            P1res = "zéro";
+            score = P1res + "-" + P2res;
+        }
+
+        if (P1point>P2point && P1point < 4)
+        {
+            if (P1point==2)
+                P1res="trente";
+            if (P1point==3)
+                P1res="quarante";
+            if (P2point==1)
+                P2res="quinze";
+            if (P2point==2)
+                P2res="trente";
+            score = P1res + "-" + P2res;
+        }
+        if (P2point>P1point && P2point < 4)
+        {
+            if (P2point==2)
+                P2res="trente";
+            if (P2point==3)
+                P2res="quarante";
+            if (P1point==1)
+                P1res="quinze";
+            if (P1point==2)
+                P1res="trente";
+            score = P1res + "-" + P2res;
+        }
+
+        if (P1point > P2point && P2point >= 3)
+        {
+            score = "avantage player1";
+        }
+
+        if (P2point > P1point && P1point >= 3)
+        {
+            score = "avantage player2";
+        }
+
+        if (P1point>=4 && P2point>=0 && (P1point-P2point)>=2)
+        {
+            score = "jeu pour player1";
+        }
+        if (P2point>=4 && P1point>=0 && (P2point-P1point)>=2)
+        {
+            score = "jeu pour player2";
+        }
+        return score;
+    }
 }
